6bb33c660b6d3b8c1c043a71c81090a55653e39d
Jamal Frank <jamaljfrank@gmail.com>
2020-02-24 21:38:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
+  get '/recipes/new' do 
+    erb :new
+  end
+  
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show 
@@ -28,9 +32,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
   
-  get '/recipes/new' do 
-    erb :new
-  end
+  
 
   post '/recipes' do 
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
